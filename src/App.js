ec10691ef8819d3f5bf22e9dbce69b05fa439458
sabbirshawon <sabbirs93@gmail.com>
2019-09-25 12:42:58 +0600

    reactjs-state

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Sabbir" bName="সাব্বির">
+      <Message />
+      {/* <Greet name="Sabbir" bName="সাব্বির">
         <button>Click</button>
       </Greet>
       <Greet name="Ahmed" bName="আহমদ"/>
       <Greet name="Shawon" bName="শাওন"/>
       <Welcome name="Sabbir" bName="সাব্বির"/>
-      {/* <Hello /> */}
+      <Hello /> */}
     </div>
   );
 }
